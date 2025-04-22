4d6da2677eec790a7965fdcb45caef07d52504eb
Mikołaj Sienicki <s33617@pjwstk.edu.pl>
2025-04-22 18:52:47 +0200

    zad 3 i 4

---
+++ Zad7Animal/src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    public void sleep(){
+        System.out.println("Sleeping...");
+    }
+    public void makeNoise(){
+        System.out.println("Making noise...");
+    }
+    public void roam(){
+        System.out.println("zwierzęta");
+    }
+
+}
